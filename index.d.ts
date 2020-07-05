9a857893856a6fa1a27d1c24cf82c0b7e1622cb6
Mahi-Uddin Zihad <zihadmahiuddin@gmail.com>
2020-07-06 02:52:41 +0600

    Added type definitions

---
+++ index.d.ts
@@ -0,0 +1,28 @@
+declare module "emoji-flags" {
+  export type CountryData = {
+    code: string;
+    emoji: string;
+    unicode: string;
+    name: string;
+    title: string;
+    dialCode: string;
+  };
+
+  type EmojiFlagsType = {
+    data: CountryData[];
+    emojis: string[];
+    codes: string[];
+    names: string[];
+    unicodes: string[];
+    countryCode(countryCode: string): CountryData;
+    [countryCode: string]:
+      | CountryData
+      | CountryData[]
+      | string[]
+      | ((countryCode: string) => CountryData);
+  };
+
+  const EmojiFlags: EmojiFlagsType;
+
+  export default EmojiFlags;
+}
